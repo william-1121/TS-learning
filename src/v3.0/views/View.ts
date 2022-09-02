384f3ab8baf36e92bec3436b3aa998477746d021
高晓娟 <bw134178@gmail.com>
2022-09-02 18:10:41 +0800

    feature: extending with Generic Constraints

---
+++ src/v3.0/views/View.ts
@@ -0,0 +1,46 @@
+import {Model} from "../models/Model";
+
+interface ModelForView {
+  on(eventName: string, callback: () => void): void
+}
+
+export abstract class View<T extends Model<K>, K> {
+// export abstract class View<T extends ModelForView>
+
+  constructor(public parent: Element, public model: T) {
+    this.bindModel();
+  }
+
+  abstract eventsMap(): {[key: string]: () => void}
+  abstract template(): string
+
+  bindModel(): void {
+    this.model.on('change', ()=> {
+      this.render();
+    })
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+
+  }
+
+  render(): void {
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template()
+
+    this.bindEvents(templateElement.content)
+
+    this.parent.append(templateElement.content)
+  }
+}
